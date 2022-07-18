89e95ddb33ed8c704fc24d68ff311a63f794e77a
Mihai Verzan <rtyh12@gmail.com>
2022-07-19 01:02:50 +0300

    Initial commit

---
+++ sprout.py
@@ -0,0 +1,17 @@
+import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv('TOKEN')
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+client = MyClient()
+client.run(TOKEN)
